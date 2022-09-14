2a25a3a074bcfb25d891f4c902b7bbffe22a0c6b
PurpleJesus521 <104554312+PurpleJesus521@users.noreply.github.com>
2022-09-14 12:31:10 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function openTab(evt,tabName){
+    //Variabler
+    var i, tabcontent, tablinks;
+    
+    //Tar alla element som ingår i classen "tabcontent" och gömmer dom
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    //Tar alla element med classen "tabcontent" och tar bort classen "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    //Visa nuvarande tab och ge classen "active" till den
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
